12ceb783ef9b8ecf69ae503d5f08c3557bd74856
Yifan Zhong <yzhong5@berkeley.edu>
2023-03-13 13:49:30 -0700

    Build signup and login page for local storage

--- frontend/app.py
+++ frontend/app.py
@@ -1,5 +1,7 @@
-from flask import Flask, flash, redirect, request, render_template, url_for
+import functools
+from flask import Flask, abort, flash, redirect, request, render_template, url_for, session
 from werkzeug.utils import secure_filename
+from passlib.hash import pbkdf2_sha256
 import os
 import pickle
 import tensorflow as tf
@@ -10,10 +12,21 @@ import coremltools as ct
 ALLOWED_EXTENSIONS = ['png', 'jpg', 'jpeg']
 UPLOAD_FOLDER = './static/images'
 
+users = {}
+
 
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def login_required(route):
+    @functools.wraps(route)
+    def route_wrapper(*args, **kwargs):
+        if not session.get("email"):
+            return redirect(url_for("login"))
+        return route(*args, **kwargs)
+
+    return route_wrapper
 
 
 def run_model():
@@ -39,6 +52,7 @@ def create_app():
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
     @app.route("/", methods=["POST", "GET"])
+    @login_required
     def home():
         if (request.method == "POST"):
             if 'file' not in request.files:
@@ -55,15 +69,40 @@ def create_app():
                 print(run_model())
                 # return redirect(url_for('download_file', name=filename))
                 return redirect(request.url)
-            
+
         return render_template("home.html")
-    
+
     @app.route("/login", methods=["GET", "POST"])
     def login():
-        return render_template("login.html")
-    
+        # session.clear()
+        email = ""
+
+        if request.method == "POST":
+            email = request.form.get("email")
+            password = request.form.get("password")
+
+            # if pbkdf2_sha256.verify(password, users.get(email)):
+            if users.get(email) == password:
+                session["email"] = email
+                return redirect(url_for("home"))
+            flash("Incorrect email or password!")
+        return render_template("login.html", email=email)
+
     @app.route("/signup", methods=["GET", "POST"])
     def signup():
+        if request.method == "POST":
+            name = request.form.get("name")
+            email = request.form.get("email")
+            password = request.form.get("password")
+
+            # users[email] = pbkdf2_sha256(password)
+            users[email] = password
+
+            flash("Successfully signed up.")
+            print(users)
+
+            return redirect(url_for("login"))
+
         return render_template("signup.html")
-    
+
     return app
