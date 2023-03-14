ae78ae37a4642dcd00b9b9d5cc50845e5b4e879d
SebastianTianying <tianyingzhu6@gmail.com>
2023-03-13 23:30:41 -0700

    preprocess predict

--- inceptionv3-OCT2.py
+++ inceptionv3-OCT2.py
@@ -1,11 +1,14 @@
 import os
-import pickle
 import tensorflow as tf
+import keras
 
+from keras import layers
+from keras import models
 from keras_preprocessing.image import ImageDataGenerator
-from tensorflow.keras.applications.resnet50 import ResNet50
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dense, Flatten, GlobalAveragePooling2D
+from tensorflow.keras.applications.inception_v3 import InceptionV3
+from tensorflow.keras.optimizers import RMSprop
 
 import numpy as np
 
@@ -20,37 +23,43 @@ validation_datagen = ImageDataGenerator(rescale=1/255)
 # Flow training images in batches of 120 using train_datagen generator
 train_generator = train_datagen.flow_from_directory(
         '/Users/seb/Downloads/OCT-Binary/train',  # This is the source directory for training images
-        target_size=(224, 224),  # All images will be resized to 224x224 as required by alexnet
+        target_size=(150, 150),  # All images will be resized to 224x224 as required by alexnet
         batch_size=20,
         # Use binary labels
         class_mode='binary')
 
-print(train_generator.class_indices)
-
 # Flow validation images in batches of 19 using valid_datagen generator
 validation_generator = validation_datagen.flow_from_directory(
         '/Users/seb/Downloads/OCT-Binary/test',  # This is the source directory for training images
-        target_size = (224, 224),  # All images will be resized to 224x224 as required by alexnet
+        target_size = (150, 150),  # All images will be resized to 224x224 as required by alexnet
         batch_size = 20,
         # Use binary labels
         class_mode='binary',
         shuffle=False)
 
-base_model = ResNet50(input_shape=(224, 224,3), include_top=False, weights="imagenet")
+print(train_generator.class_indices)
+
+base_model = InceptionV3(input_shape = (150, 150, 3), include_top = False, weights = 'imagenet')#for layer in base_model.layers:
+
 for layer in base_model.layers:
     layer.trainable = False
 
-base_model = Sequential()
-base_model.add(ResNet50(include_top=False, weights='imagenet', pooling='max'))
-base_model.add(Dense(1, activation='sigmoid'))
+x = layers.Flatten()(base_model.output)
+x = layers.Dense(1024, activation='relu')(x)
+x = layers.Dropout(0.2)(x)
 
-base_model.compile(optimizer = tf.keras.optimizers.SGD(learning_rate=0.0001), loss = 'binary_crossentropy', metrics = ['accuracy'])
-resnet_history = base_model.fit(train_generator, validation_data = validation_generator, steps_per_epoch = 50, epochs = 20)
+# Add a final sigmoid layer with 1 node for classification output
+x = layers.Dense(1, activation='sigmoid')(x)
 
-acc = resnet_history.history['accuracy'][-1]
-val_acc = resnet_history.history['val_accuracy'][-1]
-loss = resnet_history.history['loss'][-1]
-val_loss = resnet_history.history['val_loss'][-1]
+model = tf.keras.models.Model(base_model.input, x)
+
+model.compile(optimizer = RMSprop(lr=0.0001), loss = 'binary_crossentropy', metrics = ['accuracy'])
+inc_history = model.fit_generator(train_generator, validation_data = validation_generator, steps_per_epoch = 100, epochs = 20)
+
+acc = inc_history.history['accuracy'][-1]
+val_acc = inc_history.history['val_accuracy'][-1]
+loss = inc_history.history['loss'][-1]
+val_loss = inc_history.history['val_loss'][-1]
 
 print("Training accuracy: ", acc)
 print("Training loss: ", loss)
@@ -58,4 +67,4 @@ print("Training loss: ", loss)
 print("Validation accuracy: ", val_acc)
 print("Validation loss: ", val_loss)
 
-base_model.save('saved_model/resnet50-OCT-Binary-model')
+base_model.save('saved_model/inceptionv3-OCT-Binary-model')
---
+++ predictions/predict-resnet50-Xray.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+
+new_model = tf.keras.models.load_model('/Users/seb/Downloads/medical-imaging-analytics/saved_model/resnet152_model')
+# Check its architecture
+import numpy as np
+from keras.preprocessing import image
+
+# predicting images
+path = '/Users/seb/Downloads/medical-imaging-analytics/predictions/predict-pics/NORMAL-1212407-0001.jpeg'
+img = tf.keras.utils.load_img(path, target_size=(224, 224))
+#plt.imshow(img)
+#plt.show()
+
+img_array = tf.keras.utils.img_to_array(img)
+img_batch = np.expand_dims(img_array, axis=0)
+img_batch = img_batch.astype('float32')
+img = img_batch/255
+
+prediction = new_model.predict(img)
+print(prediction)
+
+if prediction[0] <0.5:
+    print("image shows PNEUMONIA")
+else:
+    print("image shows NORMAL")
